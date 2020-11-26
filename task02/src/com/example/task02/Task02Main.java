550645233432807852259cac8576949aa7bbde6a
lexa1329 <funin99@bk.ru>
2020-11-26 14:37:27 +0500

    task02[Фунин]

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -6,8 +6,18 @@ public class Task02Main {
     public static void main(String[] args) throws IOException {
         // чтобы протестировать свое решение, вам нужно:
         // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
-        // - направить стандартный вывод программы в файл output.test
-        // - запустить программу
-        // - и сравнить получившийся файл output.test с expected.test
+                // - направить стандартный вывод программы в файл output.test
+                // - запустить программу
+                // - и сравнить получившийся файл output.test с expected.test
+        while (System.in.available() > 0) {
+            int readByte = System.in.read();
+            System.in.mark(1);
+            int nextByteFromIn = System.in.read();
+            if (readByte != '\r' || nextByteFromIn != '\n') {
+                System.out.write(readByte);
+            }
+            System.in.reset();
+        }
+        System.out.flush();
     }
 }
