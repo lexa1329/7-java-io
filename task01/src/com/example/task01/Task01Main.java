d0b53efb246aecc8c49515c7708a41a476d0dd53
lexa1329 <funin99@bk.ru>
2020-11-26 14:29:06 +0500

    task01[Фунин]

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -1,6 +1,8 @@
 package com.example.task01;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.util.Objects;
 import java.io.InputStream;
 
 public class Task01Main {
@@ -8,14 +10,22 @@ public class Task01Main {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
 
-        /*
+
         System.out.println(checkSumOfStream(new ByteArrayInputStream(new byte[]{0x33, 0x45, 0x01})));
-        */
+
 
     }
 
     public static int checkSumOfStream(InputStream inputStream) throws IOException {
-        // your implementation here
-        return 0;
+        if(inputStream == null){
+            throw new IllegalArgumentException("stream is null");
+        }
+
+        int sum = 0, tmp;
+        while ((tmp = inputStream.read()) != -1){
+            sum = Integer.rotateLeft(sum,1) ^ tmp;
+        }
+
+        return sum;
     }
 }
